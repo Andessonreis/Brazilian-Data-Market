3efb5db771e1d67be625b951fe9a113e428356f6
andesson reis <andessonreys@gmail.com>
2023-12-24 16:16:19 -0300

    refactor: mudando o direitorio de extração para um pacote individual mais melhor modularidade

---
+++ dependencies/etl/extract/data_extraction.py
@@ -0,0 +1,10 @@
+"""
+data_extraction.py
+~~~~~~~~~~~~~~~~~~
+
+extracte é essencial para nosso pipeline de dados, realizando ETL para extrair e preparar dados de fontes diversas. 
+Executado como job em um cluster, otimiza o processamento distribuído para lidar com grandes conjuntos de dados. 
+Este packager encapsula funcionalidades críticas, contribuindo para a coesão e qualidade dos dados. 
+Foco na modularidade e eficiência, com o objetivo de fornecer insights valiosos. 
+O processo concentra-se na extração de dados de uma localização genérica, promovendo um código claro e conciso.
+"""
